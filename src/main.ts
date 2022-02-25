ffd9511581ab6e1d2e0137164bd0366aa691c264
Osama Ishaq <osama.ishaq@ibex.co>
2022-02-25 20:34:04 +0500

    CHanges

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen("https://module-federation-server.vercel.app/");
+  await app.listen(8080);
 }
 bootstrap();
